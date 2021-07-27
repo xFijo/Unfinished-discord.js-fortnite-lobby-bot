2d82317c9dbe55b8f6d7864e8107c5c819297899
xFijo <75216577+xFijo@users.noreply.github.com>
2021-07-28 00:38:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client } = require('fnbr');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { MessageEmbed } = require('discord.js');
+
+//const success = '3066993';
+//const fail = '15105570';
+
+const bot = new Client({
+  auth: {
+    authorizationCode: '',
+  },
+  debug: false
+});
+
+(async () => {
+  await bot.login();
+  console.log(`Logged on ${bot.user.displayName}`);
+})();
+
+client.on('ready', () => {
+  console.log(`[SERVER][BOT] Client succesfully logged in as ${client.user.tag}`);
+  console.log(`Watching ${client.guilds.cache.size} servers!`);
+  client.user.setPresence({ activity: { name: 'Watching Fortnite' }, status: 'idle' })
+});
+
+const accEmbed = new MessageEmbed()
+ .setColor('GREEN')
+ .setTitle('Account')
+ .addField(`Currently logged in as:`, bot.user.displaName)
+ .setTimestamp()
+
+ client.on('message', msg => {
+   if (msg == '--account') {
+     msg.channel.send(accEmbed);
+   };
+ });
+
+const mySecret = process.env['token']
+client.login(mySecret)
